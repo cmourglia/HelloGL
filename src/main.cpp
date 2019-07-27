33e3b4852c7d858e2e8bc1eb7be1b3e3049c0a57
Charly Mourglia <charly.mourglia@gmail.com>
2019-07-27 18:54:35 +0200

    Use conan, remove subdmodules, use glad instead of gl3w

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <GL/gl3w.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include <stdio.h>
@@ -13,235 +13,231 @@ bool InitializeRenderer();
 void Update(float dt);
 void Render(float dt);
 
-int g_Width, g_Height;
+int gWidth, gHeight;
 
 // http://www.glfw.org/docs/latest/input_guide.html
 void KeyCallback(GLFWwindow* Window, int Key, int Scancode, int Action, int Mods);
 void MouseButtonCallback(GLFWwindow* Window, int Button, int Action, int Mods);
 void MouseMoveCallback(GLFWwindow* Window, double X, double Y);
-// 
+//
 void FramebufferSizeCallback(GLFWwindow* Window, int Width, int Height);
 
 int main()
 {
-    GLFWwindow* Window;
-    
+    GLFWwindow* window;
+
     if (!glfwInit())
     {
         fprintf(stderr, "glfwInit() failed.\n");
         return -1;
     }
-    
-    Window = glfwCreateWindow(640, 480, "Hello, World", nullptr, nullptr);
-    if (!Window)
+
+    window = glfwCreateWindow(640, 480, "Hello, World", nullptr, nullptr);
+    if (!window)
     {
         fprintf(stderr, "glfwCreateWindow() failed.\n");
         glfwTerminate();
         return -1;
     }
-    
-    glfwMakeContextCurrent(Window);
-    glfwGetFramebufferSize(Window, &g_Width, &g_Height);
-    
-    if (gl3wInit())
+
+    glfwMakeContextCurrent(window);
+    glfwGetFramebufferSize(window, &gWidth, &gHeight);
+
+    if (!gladLoadGL())
     {
-        fprintf(stderr, "gl3wInit() failed.\n");
+        fprintf(stderr, "gladLoadGL() failed.\n");
         return -1;
     }
-    
-    if (!gl3wIsSupported(4, 5))
-    {
-        fprintf(stderr, "Opengl 4.5 not supported.\n");
-        return -1;
-    }
-    
+
     printf("OpenGL %s, GLSL %s\n", glGetString(GL_VERSION), glGetString(GL_SHADING_LANGUAGE_VERSION));
 
-    glfwSetKeyCallback(Window, KeyCallback);
-    glfwSetMouseButtonCallback(Window, MouseButtonCallback);
-    glfwSetCursorPosCallback(Window, MouseMoveCallback);
-    glfwSetFramebufferSizeCallback(Window, FramebufferSizeCallback);
-    
+    glfwSetKeyCallback(window, KeyCallback);
+    glfwSetMouseButtonCallback(window, MouseButtonCallback);
+    glfwSetCursorPosCallback(window, MouseMoveCallback);
+    glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
+
     if (!InitializeRenderer())
     {
         fprintf(stderr, "InitRenderer() failed.\n");
         return -1;
     }
-    
+
     double t0 = glfwGetTime();
     double t1;
     float dt;
-    while (!glfwWindowShouldClose(Window))
+    while (!glfwWindowShouldClose(window))
     {
         t1 = glfwGetTime();
         dt = (float)(t1 - t0);
         t0 = t1;
-        
+
         glfwPollEvents();
         Update(dt);
         Render(dt);
-        
-        glfwSwapBuffers(Window);
+
+        glfwSwapBuffers(window);
     }
-    
+
     return 0;
 }
 
-static GLuint g_TriangleVBO;
-static GLuint g_TriangleVAO;
+static GLuint gTriangleVBO;
+static GLuint gTriangleVAO;
 
-static GLuint g_ShaderProgram;
+static GLuint gShaderProgram;
 
-static const char* g_VertexShader =
-"#version 450 core\n"
-"layout (location = 0) in vec3 in_Position;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = vec4(in_Position, 1.0);\n"
-"}\n";
+static const char* gVertexShaderSrc = R"glsl(
+#version 450 core
+layout (location = 0) in vec3 in_Position;
+void main()
+{
+    gl_Position = vec4(in_Position, 1.0);
+}
+)glsl";
 
-static const char* g_FragmentShader =
-"#version 450 core\n"
-"layout (location = 0) out vec4 out_Color;\n"
-"void main()\n"
-"{\n"
-"    out_Color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-"}\n";
+static const char* gFragmentShaderSrc = R"glsl(
+#version 450 core
+layout (location = 0) out vec4 out_Color;
+void main()
+{
+    out_Color = vec4(1.0, 0.0, 0.0, 1.0);
+}
+)glsl";
 
-bool CheckShader(GLuint Shader)
+bool CheckShader(GLuint pShader)
 {
     GLint CompileStatus;
-    
+
     GLchar InfoLog[512];
-    glGetShaderiv(Shader, GL_COMPILE_STATUS, &CompileStatus);
-    
+    glGetShaderiv(pShader, GL_COMPILE_STATUS, &CompileStatus);
+
     if (!CompileStatus)
     {
-        glGetShaderInfoLog(Shader, 512, nullptr, InfoLog);
+        glGetShaderInfoLog(pShader, 512, nullptr, InfoLog);
         fprintf(stderr, "Shader compilation error : %s\n", InfoLog);
     }
-    
+
     return (bool)CompileStatus;
 }
 
-bool CheckProgram(GLuint Program)
+bool CheckProgram(GLuint pProgram)
 {
     GLint LinkStatus;
-    
+
     GLchar InfoLog[512];
-    glGetProgramiv(Program, GL_LINK_STATUS, &LinkStatus);
-    
+    glGetProgramiv(pProgram, GL_LINK_STATUS, &LinkStatus);
+
     if (!LinkStatus)
     {
-        glGetProgramInfoLog(Program, 512, nullptr, InfoLog);
+        glGetProgramInfoLog(pProgram, 512, nullptr, InfoLog);
         fprintf(stderr, "Program link error : %s\n", InfoLog);
     }
-    
+
     return (bool)LinkStatus;
 }
 
 bool InitializeRenderer()
 {
-    bool Result = true;
-    
+    bool result = true;
+
     // Setup buffers
-    glGenVertexArrays(1, &g_TriangleVAO);
-    glBindVertexArray(g_TriangleVAO);
-    
+    glGenVertexArrays(1, &gTriangleVAO);
+    glBindVertexArray(gTriangleVAO);
+
     GLfloat vertices[] =
     {
         -0.5f, -0.5f, 0.0f,
         0.5, -0.5f, 0.0f,
         0.0f, 0.5f, 0.0f,
     };
-    
-    glGenBuffers(1, &g_TriangleVBO);
-    glBindBuffer(GL_ARRAY_BUFFER, g_TriangleVBO);
+
+    glGenBuffers(1, &gTriangleVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, gTriangleVBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof vertices, vertices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)nullptr);
     glEnableVertexAttribArray(0);
-    
+
     glBindVertexArray(0);
-    
+
     // Compile shaders
-    GLuint VertexShader;
-    VertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(VertexShader, 1, &g_VertexShader, nullptr);
-    glCompileShader(VertexShader);
-    if (!CheckShader(VertexShader))
+    GLuint vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &gVertexShaderSrc, nullptr);
+    glCompileShader(vertexShader);
+    if (!CheckShader(vertexShader))
     {
-        Result = false;
-        return Result;
+        result = false;
+        return result;
     }
-    
-    GLuint FragmentShader;
-    FragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(FragmentShader, 1, &g_FragmentShader, nullptr);
-    glCompileShader(FragmentShader);
-    if (!CheckShader(FragmentShader))
+
+    GLuint fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &gFragmentShaderSrc, nullptr);
+    glCompileShader(fragmentShader);
+    if (!CheckShader(fragmentShader))
     {
-        Result = false;
-        return Result;
+        result = false;
+        return result;
     }
-    
+
     // Link shaders
-    g_ShaderProgram = glCreateProgram();
-    glAttachShader(g_ShaderProgram, VertexShader);
-    glAttachShader(g_ShaderProgram, FragmentShader);
-    glLinkProgram(g_ShaderProgram);
-    if (!CheckProgram(g_ShaderProgram))
+    gShaderProgram = glCreateProgram();
+    glAttachShader(gShaderProgram, vertexShader);
+    glAttachShader(gShaderProgram, fragmentShader);
+    glLinkProgram(gShaderProgram);
+    if (!CheckProgram(gShaderProgram))
     {
-        Result = false;
-        return Result;
+        result = false;
+        return result;
     }
-    
-    glDetachShader(g_ShaderProgram, VertexShader);
-    glDeleteShader(VertexShader);
-    
-    glDetachShader(g_ShaderProgram, FragmentShader);
-    glDeleteShader(FragmentShader);
-    
-    return Result;
+
+    glDetachShader(gShaderProgram, vertexShader);
+    glDeleteShader(vertexShader);
+
+    glDetachShader(gShaderProgram, fragmentShader);
+    glDeleteShader(fragmentShader);
+
+    return result;
 }
 
-void Update(float dt)
+void Update(float pDeltaTime)
 {
 }
 
-void Render(float dt)
+void Render(float pDeltaTime)
 {
     glClear(GL_COLOR_BUFFER_BIT);
     // Set viewport
-    glViewport(0, 0, g_Width, g_Height);
-    
+    glViewport(0, 0, gWidth, gHeight);
+
     // Use program
-    glUseProgram(g_ShaderProgram);
-    
+    glUseProgram(gShaderProgram);
+
     // Render triangle
-    glBindVertexArray(g_TriangleVAO);
+    glBindVertexArray(gTriangleVAO);
     glDrawArrays(GL_TRIANGLES, 0, 3);
     glBindVertexArray(0);
 }
 
-void KeyCallback(GLFWwindow* Window, int Key, int Scancode, int Action, int Mods)
+void KeyCallback(GLFWwindow* pWindow, int Key, int Scancode, int Action, int Mods)
 {
     if (Key == GLFW_KEY_ESCAPE)
     {
-        glfwSetWindowShouldClose(Window, true);
+        glfwSetWindowShouldClose(pWindow, true);
     }
 }
 
-void MouseButtonCallback(GLFWwindow* Window, int Button, int Action, int Mods)
+void MouseButtonCallback(GLFWwindow* pWindow, int pButton, int pAction, int pMods)
 {
 }
 
-void MouseMoveCallback(GLFWwindow* Window, double X, double Y)
+void MouseMoveCallback(GLFWwindow* pWindow, double pX, double pY)
 {
 }
 
-void FramebufferSizeCallback(GLFWwindow* Window, int Width, int Height)
+void FramebufferSizeCallback(GLFWwindow* pWindow, int pWidth, int pHeight)
 {
-    g_Width = Width;
-    g_Height = Height;
+    gWidth = pWidth;
+    gHeight = pHeight;
 }
